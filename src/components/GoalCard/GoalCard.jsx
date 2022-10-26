8fd120801dd83d8b97d62d68783fc1d491ee0307
Jesse Nicolas <jn@nevada.unr.edu>
2022-10-25 20:41:54 -0700

    functional delete

--- src/components/GoalCard/GoalCard.jsx
+++ src/components/GoalCard/GoalCard.jsx
@@ -1,7 +1,24 @@
 const GoalCard = (props) => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleDeleteComment(props.id, props.goal._id)
+    const newGoals = props.profile.goals.filter(goal => goal._id !== props.goal._id)
+    props.setProfile({...props.profile, goals: newGoals})
+  }
+
   return (  
     <>
       <h2>{props.goal.content}</h2>
+      {props.goal.content && (
+        <div>
+      <form onSubmit={handleSubmit}>
+        <button type='submit'>Delete</button>
+      </form>
+      <form>
+      <button>Edit</button>
+      </form>
+      </div>
+      )}
     </>
   );
 }
