72e815832063680f3a06441c1d5b8aa67ce627c1
Chrsteffen <Chrsteffen1@gmail.com>
2022-10-27 22:15:34 -0400

    style edit exercise form

--- src/pages/EditExercise/EditExercise.jsx
+++ src/pages/EditExercise/EditExercise.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useLocation } from "react-router-dom"
+import styles from './EditExercise.module.css'
 
 const EditExercise = (props) => {
   const { state } = useLocation()
@@ -15,55 +16,64 @@ const EditExercise = (props) => {
   }
   
   return (
-    <main>
-      <h2>Edit Exercise</h2>
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <label htmlFor="name-input">Exercise</label>
-        <input
-          required
-          type="text"
-          name="name"
-          id="name-input"
-          value={form.name}
-          placeholder="Name"
-          onChange={handleChange}
-        />
-        <label htmlFor="reps-input">Reps</label>
-				<input
-          required
-          type="number"
-          name="reps"
-          id="reps-input"
-          value={form.reps}
-          placeholder="Reps"
-          onChange={handleChange}
-        />
-        <label htmlFor="muscle-input">Muscle</label>
-        <select
-          required
-          name="muscle"
-          id="muscle-input"
-          value={form.muscle}
-          onChange={handleChange}
-        >
-          <option value="upper back">Upper Back</option>
-          <option value="lower back">Lower Back</option>
-          <option value="chest">Chest</option>
-          <option value="abs">Abs</option>
-          <option value="arms">Arms</option>
-          <option value="legs">Legs</option>
-        </select>
-        <label htmlFor="equipment-input">Equipment</label>
-        <input
-          required
-          type="text"
-          name="equipment"
-          id="equipment-input"
-          value={form.equipment}
-          placeholder="Equipment"
-          onChange={handleChange}
-        />
-        <button type="submit">Update</button>
+    <main className={styles.container}>
+      <form className={styles.container} onSubmit={handleSubmit} autoComplete="off">
+        <h2>Create an Exercise</h2>
+        <div className={styles.inputContainer}>
+            <label className={styles.label} htmlFor="name-input">Exercise</label>
+            <input
+              required
+              type="text"
+              name="name"
+              id="name-input"
+              value={form.name}
+              placeholder="Name"
+              onChange={handleChange}
+            />
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}  htmlFor="reps-input">Reps</label>
+          <input
+            required
+            type="number"
+            name="reps"
+            id="reps-input"
+            value={form.reps}
+            placeholder="Reps"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+        <label className={styles.label}  htmlFor="equipment-input">Equipment</label>
+          <input
+            required
+            type="text"
+            name="equipment"
+            id="equipment-input"
+            value={form.equipment}
+            placeholder="Equipment"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}  htmlFor="muscle-input">Muscle</label>
+          <select
+            required
+            name="muscle"
+            id="muscle-input"
+            value={form.muscle}
+            onChange={handleChange}
+            className={styles.select}
+            >
+            <option className={styles.option} value="upper back">Upper Back</option>
+            <option value="lower back">Lower Back</option>
+            <option value="chest">Chest</option>
+            <option value="abs">Abs</option>
+            <option value="arms">Arms</option>
+            <option value="legs">Legs</option>
+          </select>
+        </div>
+        <button className={styles.button}type="submit">SUBMIT</button>
       </form>
     </main>
   )
